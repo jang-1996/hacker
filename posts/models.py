b6d2548a104cedc12e812b524603a73c2cb806b2
altu-1996 <altu1996@naver.com>
2020-08-05 20:30:09 +0900

    first commit

---
+++ posts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50, null=False)
+    content = models.TextField()
+    view_count = models.IntegerField(default = 0)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    image = models.ImageField(upload_to = 'images/', null = True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, null=True)
